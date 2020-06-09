da3bd3bf45c5ffb7f4c4b29e7f9d2c649a431588
Peter Vesterkær <pve@novicell.dk>
2020-06-09 10:54:31 +0200

    OS2ULF-133: Header, logo  and graphics changes

--- template.php
+++ template.php
@@ -1,1 +1,9 @@
 <?php
+
+/**
+ * Implements hook_preprocess_page().
+ */
+function ulf_randers_preprocess_page(&$variables) {
+  // Hamburger icon.
+  $variables['hamburger_icon_path'] = $variables['directory'];
+}
